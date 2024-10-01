f4fc55ff2627346dcb2e554af34e94596db8197f
Headcase <Headcase@DESKTOP-VJNKEN5>
2024-10-01 17:06:15 +1000

    Добавьте файлы проекта.

---
+++ RemainderOfDivision/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel;
+
+namespace RemainderOfDivision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***Программа для вывода результатов операции деления***");
+            var a = ParseInt("Введите делимое: ");
+            var b = ParseInt("Введите делитель: ");
+
+            if (b == 0)
+            {
+                Console.WriteLine("Делить на ноль нельзя!");
+                return;
+            }
+
+            var quotient = a / b;
+            var reminder = a % b;
+
+            Console.WriteLine($"(при условии что a = {a} и b = {b}): {a}/{b} = {quotient} остаток {reminder}");
+
+        }
+
+        static int ParseInt(string message = "")
+        {
+            Console.WriteLine(message);
+            return Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
